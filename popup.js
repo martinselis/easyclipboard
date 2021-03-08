c20ab0f56c65a4131c3d5f4be3060e626216d3ef
Martin Selis <sileshs@gmail.com>
2021-03-08 11:06:17 +0000

    Clean up popup script  - use db facade going forward  - remove global scope variables

--- popup.js
+++ popup.js
@@ -1,8 +1,8 @@
 let uniqueCategories;
 
 // Add/Update tabs
-let addTab = document.getElementById("addTab")
-let updateTab = document.getElementById("updateTab")
+let addTab = document.getElementById("addTab");
+let updateTab = document.getElementById("updateTab");
 
 // Blurb
 let displayBlurbList = document.getElementsByClassName("displayBlurb");
@@ -17,300 +17,286 @@ let addButton = document.getElementById("addText");
 let updateButton = document.getElementById("updateText");
 let deleteButton = document.getElementById("deleteText");
 
-//Confirm blurbs
-let confirmationWindow = document.getElementsByClassName("hideConfirm")[0]
+const dbGetFacade = chrome.extension.getBackgroundPage().dbGetFacade;
+const saveInDB = chrome.extension.getBackgroundPage().saveInDB;
+
+// Confirm blurbs
+let confirmationWindow = document.getElementsByClassName("hideConfirm")[0];
+
+function log(logInfo, logInfo2) {
+  chrome.extension.getBackgroundPage().console.log(logInfo, logInfo2);
+}
 
 // Delete the selected blurb
-deleteButton.addEventListener("click", deleteSelection)
+deleteButton.addEventListener("click", deleteSelection);
 function deleteSelection() {
-    let blurbSelected = document.getElementsByClassName("activeBlurb")[0]
-    let selectedID = blurbSelected.lastChild.textContent
-    chrome.storage.sync.get(['blurbs'], function(resultDB) {
-      for (var i = 0; i < resultDB["blurbs"].length; i++) {
-         if (resultDB["blurbs"][i]["blurbID"] == selectedID) {
-          resultDB["blurbs"].splice(i, 1)
-          saveInDB(resultDB, "Deleted")
-          }
-          clearInputs();
-          hideButton(updateButton)
-          updateTab.className = "";
-          addTab.className = "active";
+  let blurbSelected = document.getElementsByClassName("activeBlurb")[0];
+  let selectedID = blurbSelected.lastChild.textContent;
+
+  dbGetFacade("blurbs", function (resultDB) {
+    for (let i = 0; i < resultDB["blurbs"].length; i++) {
+      if (resultDB["blurbs"][i]["blurbID"] == selectedID) {
+        resultDB["blurbs"].splice(i, 1);
+        saveInDB(resultDB, document, confirmationWindow, "Deleted");
       }
-    })
-  }
 
-// Check if input fields are empty
-function verifyFields() {
-  var blurbCategory = blurbCategoryField.value
-  var blurbTitle = blurbTitleField.value
-  var blurbText = blurbTextField.value
-  if (blurbCategoryField.value != "" && blurbTitleField.value != "" && blurbTextField.value != "") {
-    return true
-  } else {
-    return false
-  }
+      clearInputs();
+      hideButton(updateButton);
+      updateTab.className = "";
+      addTab.className = "active";
+    }
+  });
 }
+
 // Modify existing blurb
-updateButton.addEventListener("click", updateBlurb)
+updateButton.addEventListener("click", updateBlurb);
 function updateBlurb() {
-  var fieldsNotEmpty = verifyFields()
+  const fieldsNotEmpty = verifyFields();
+
   if (fieldsNotEmpty) {
-    let blurbSelected = document.getElementsByClassName("activeBlurb")[0]
-    let selectedID = blurbSelected.lastChild.textContent
-    chrome.storage.sync.get(['blurbs'], function(resultDB) {
-      for (var i = 0; i < resultDB["blurbs"].length; i++) {
-         if (resultDB["blurbs"][i]["blurbID"] == selectedID) {
-           resultDB["blurbs"].splice(i, 1)
-
-           saveInDB(resultDB, "Updated")
-           break
-         }
-       }
-       addBlurb()
-     })
-
+    let blurbSelected = document.getElementsByClassName("activeBlurb")[0];
+    let selectedID = blurbSelected.lastChild.textContent;
+    dbGetFacade("blurbs", (resultDB) => {
+      for (let i = 0; i < resultDB["blurbs"].length; i++) {
+        if (resultDB["blurbs"][i]["blurbID"] == selectedID) {
+          resultDB["blurbs"].splice(i, 1);
+          saveInDB(resultDB, document, confirmationWindow, "Updated");
+          break;
+        }
+      }
+      addBlurb();
+    });
   } else {
-    throwError()
+    throwError();
   }
 }
 
 // Add new blurb to DB
-addButton.addEventListener("click", addBlurb)
+addButton.addEventListener("click", addBlurb);
 
 function addBlurb() {
-  var addNew = true;
-  var uniqueID = Date.now()
-  var addition = { category: blurbCategoryField.value, title: blurbTitleField.value, blurb: blurbTextField.value, blurbID: uniqueID}
-  var fieldsNotEmpty = verifyFields()
+  let addNew = true;
+  const uniqueID = Date.now();
+  const addition = {
+    category: blurbCategoryField.value,
+    title: blurbTitleField.value,
+    blurb: blurbTextField.value,
+    blurbID: uniqueID,
+  };
+
+  const fieldsNotEmpty = verifyFields();
+
   if (fieldsNotEmpty) {
-    chrome.storage.sync.get(['blurbs'], function(resultDB) {
-      if (!resultDB['blurbs'] || resultDB['blurbs'].length == 0) {
-        resultDB["blurbs"] = []
+    dbGetFacade("blurbs", (resultDB) => {
+      if (!resultDB["blurbs"] || resultDB["blurbs"].length == 0) {
+        resultDB["blurbs"] = [];
         addNew = true; // Allows to add blurb if db empty
       } else {
-        for (var i = 0; i < resultDB['blurbs'].length; i++) {
-          if (resultDB["blurbs"][i]["category"] == blurbCategoryField.value && resultDB["blurbs"][i]["title"] == blurbTitleField.value) {
-            duplicateError()
-            addNew = false // If duplicate exists, dont allow to add blurb
+        for (let i = 0; i < resultDB["blurbs"].length; i++) {
+          if (
+            resultDB["blurbs"][i]["category"] == blurbCategoryField.value &&
+            resultDB["blurbs"][i]["title"] == blurbTitleField.value
+          ) {
+            duplicateError();
+            addNew = false; // If duplicate exists, dont allow to add blurb
           }
         }
-        }
-      if (addNew) { // Allow update DB if no duplicicates
-        resultDB["blurbs"].push(addition)
-        saveInDB(resultDB, "Added")
+      }
+
+      if (addNew) {
+        // Allow update DB if no duplicicates
+        resultDB["blurbs"].push(addition);
+        saveInDB(resultDB, document, confirmationWindow, "Added");
         clearInputs();
       }
-    })
+    });
   }
 }
-/*
-    var uniqueID = Date.now()
-    var addition = { category: blurbCategoryField.value, title: blurbTitleField.value, blurb: blurbTextField.value, blurbID: uniqueID}
 
-  // Adding blurb to DB
-    chrome.storage.sync.get(['blurbs'], function(resultDB) {
-        if (!resultDB['blurbs']) {
-          resultDB["blurbs"] = []
-        }
-        resultDB["blurbs"].push(addition)
-        saveInDB(resultDB, "Added")
-        clearInputs();
-      })
-  } else {
-    throwError()
-  } */
+// Check if input fields are empty
+function verifyFields() {
+  const blurbCategory = blurbCategoryField.value;
+  const blurbTitle = blurbTitleField.value;
+  const blurbText = blurbTextField.value;
 
-
-// Store data in database
-function saveInDB(resultDB, action_confirm) {
-  chrome.storage.sync.set({blurbs: resultDB["blurbs"]}, function(result) {
-    let confirmText = document.createElement("p")
-    confirmText.textContent = action_confirm;
-    confirmationWindow.innerHTML = '';
-    confirmationWindow.appendChild(confirmText)
-    confirmationWindow.className += " showConfirm"
-    setTimeout(function(){ confirmationWindow.className = "hideConfirm"}, 2500)
-
-  })
+  return blurbCategory != "" && blurbTitle != "" && blurbText != "";
 }
 
 function throwError() {
-  var hideError = document.getElementsByClassName("hideError")[0]
-  hideError.className += " showError"
-  setTimeout(function(){ hideError.className = "hideError"}, 3000)
-
-
+  let hideError = document.getElementsByClassName("hideError")[0];
+  hideError.className += " showError";
+  setTimeout(function () {
+    hideError.className = "hideError";
+  }, 3000);
 }
 
 // Make sure duplicate title does not exist
 function checkDuplicates() {
-    chrome.storage.sync.get(['blurbs'], function(resultDB) {
-      console.log(resultDB['blurbs'])
-      if (resultDB['blurbs'].length == 0) {
-        return true
-      } else {
-        for (var i = 0; i < resultDB['blurbs'].length; i++) {
-          if (resultDB["blurbs"][i]["title"] == blurbTitleField.value) {
-            var hideDuplicate = document.getElementsByClassName("hideDuplicate")[0];
-            hideDuplicate.className += " showError"
-            setTimeout(function(){ hideDuplicate.className = "hideDuplicate"}, 3000)
-            return false
-          } else {
-            return true}
+  dbGetFacade("blurbs", function (resultDB) {
+    if (resultDB["blurbs"].length == 0) {
+      return true;
+    } else {
+      for (let i = 0; i < resultDB["blurbs"].length; i++) {
+        if (resultDB["blurbs"][i]["title"] == blurbTitleField.value) {
+          let hideDuplicate = document.getElementsByClassName(
+            "hideDuplicate"
+          )[0];
+          hideDuplicate.className += " showError";
+          setTimeout(function () {
+            hideDuplicate.className = "hideDuplicate";
+          }, 3000);
+          return false;
+        } else {
+          return true;
         }
       }
-    })
+    }
+  });
 }
 
 function duplicateError() {
-  var hideDuplicate = document.getElementsByClassName("hideDuplicate")[0];
-  hideDuplicate.className += " showError"
-  setTimeout(function(){ hideDuplicate.className = "hideDuplicate"}, 3000)
+  let hideDuplicate = document.getElementsByClassName("hideDuplicate")[0];
+  hideDuplicate.className += " showError";
+  setTimeout(function () {
+    hideDuplicate.className = "hideDuplicate";
+  }, 3000);
 }
 // Create right-side read section to display existing Blurbs
-var displaySection = document.getElementById("displaySection")
-
+let displaySection = document.getElementById("displaySection");
 function displayRightSide() {
-  displaySection.innerHTML = '';
-  chrome.storage.sync.get(['blurbs'], function(resultDB) {
+  displaySection.innerHTML = "";
+
+  dbGetFacade("blurbs", (resultDB) => {
     try {
-    uniqueCategories = unique(resultDB["blurbs"], "category") // List of unique categories
+      // log("right side data", resultDB);
+      if (resultDB.blurbs.length === 0) return;
+      uniqueCategories = unique(resultDB["blurbs"], "category"); // List of unique categories
 
-      uniqueCategories.forEach(function(element) {
-
+      uniqueCategories.forEach(function (element) {
         // Create categories (displayed once)
-        var newCategory = document.createElement("div");
+        const newCategory = document.createElement("div");
         newCategory.className = "displayBlurbCategory";
-        var categoryName = document.createElement("div");
+        const categoryName = document.createElement("div");
         categoryName.className = "categoryTitle";
         categoryName.textContent = element;
-        newCategory.appendChild(categoryName) // Setting category name at the top;
+        newCategory.appendChild(categoryName); // Setting category name at the top;
 
-        for (var i = 0; i < resultDB["blurbs"].length; i++) {
+        for (let i = 0; i < resultDB["blurbs"].length; i++) {
           if (resultDB["blurbs"][i]["category"] == element) {
-
             // Create new blurb entry
-            var newBlurbDiv = document.createElement("div");
-            newBlurbDiv.className = "displayBlurb"
-            var newP = document.createElement("p");
-            newP.textContent = resultDB["blurbs"][i]["title"] + ": " + resultDB["blurbs"][i]["blurb"];
-            newBlurbDiv.appendChild(newP)
+            const newBlurbDiv = document.createElement("div");
+            newBlurbDiv.className = "displayBlurb";
+            const newP = document.createElement("p");
+            newP.textContent =
+              resultDB["blurbs"][i]["title"] +
+              ": " +
+              resultDB["blurbs"][i]["blurb"];
+            newBlurbDiv.appendChild(newP);
 
             // Create div with unique ID
-            var uniqueDiv = document.createElement("div");
-            uniqueDiv.className = "uniqueDiv"
-            uniqueDiv.textContent = resultDB["blurbs"][i]["blurbID"]
-            newBlurbDiv.appendChild(uniqueDiv)
+            const uniqueDiv = document.createElement("div");
+            uniqueDiv.className = "uniqueDiv";
+            uniqueDiv.textContent = resultDB["blurbs"][i]["blurbID"];
+            newBlurbDiv.appendChild(uniqueDiv);
 
-            blurbListeners(newBlurbDiv)
-            newCategory.appendChild(newBlurbDiv)
+            blurbListeners(newBlurbDiv);
+            newCategory.appendChild(newBlurbDiv);
           }
         }
-        displaySection.appendChild(newCategory)
-      })
-    } catch {
-
-    }
-    }
-  )
+        displaySection.appendChild(newCategory);
+      });
+    } catch {}
+  });
 }
 
 // Add listeners for right side blurb selection
 function blurbListeners(newBlurbDiv) {
-  newBlurbDiv.addEventListener("click", blurbClicked)
+  newBlurbDiv.addEventListener("click", blurbClicked);
 }
 
 // Right-side blurb onclick listener functions
 function blurbClicked() {
-
-  clearBlurbColors()
+  clearBlurbColors();
   this.className = "displayBlurb activeBlurb"; // Clicked blurb now visible
   updateTab.className = "active";
   addTab.className = "";
-  var act = this
-  updateFields(act)
-  hideButton(addButton)
-
+  const act = this;
+  updateFields(act);
+  hideButton(addButton);
 }
 
 // Function to fire when "New Blurb Template" clicked
-addTab.addEventListener("click", function() {
-  addTab.className = "active"
+addTab.addEventListener("click", function () {
+  addTab.className = "active";
   updateTab.className = "";
-  clearBlurbColors()
-  clearInputs()
-  hideButton(updateButton)
-})
+  clearBlurbColors();
+  clearInputs();
+  hideButton(updateButton);
+});
 
 // When blurb selected, update input fields
 function updateFields(act) {
-  var blurbIDtoSearch = act.lastChild.textContent
-  chrome.storage.sync.get(['blurbs'], function(resultDB) {
-    for (var i = 0; i < resultDB["blurbs"].length; i++) {
+  const blurbIDtoSearch = act.lastChild.textContent;
+  dbGetFacade("blurbs", (resultDB) => {
+    for (let i = 0; i < resultDB["blurbs"].length; i++) {
       if (resultDB["blurbs"][i]["blurbID"] == blurbIDtoSearch) {
-        blurbCategoryField.value = resultDB["blurbs"][i]["category"]
-        blurbTitleField.value = resultDB["blurbs"][i]["title"]
-        blurbTextField.value = resultDB["blurbs"][i]["blurb"]
-        break
+        blurbCategoryField.value = resultDB["blurbs"][i]["category"];
+        blurbTitleField.value = resultDB["blurbs"][i]["title"];
+        blurbTextField.value = resultDB["blurbs"][i]["blurb"];
+        break;
       }
     }
-  })
-
-
+  });
 }
 
 // Clear inputs
 function clearInputs() {
-  blurbCategoryField.value = '';
-  blurbTitleField.value = '';
-  blurbTextField.value = '';
+  blurbCategoryField.value = "";
+  blurbTitleField.value = "";
+  blurbTextField.value = "";
 }
 
 // Switch Add/Update buttons
 function hideButton(button) {
-    if (button == updateButton) {
-      updateButton.className = "hideButton";
-      deleteButton.className = "hideButton";
-      addButton.className = '';
-    } else {
-      updateButton.className = "";
-      deleteButton.className = "";
-      addButton.className = "hideButton";
+  if (button == updateButton) {
+    updateButton.className = "hideButton";
+    deleteButton.className = "hideButton";
+    addButton.className = "";
+  } else {
+    updateButton.className = "";
+    deleteButton.className = "";
+    addButton.className = "hideButton";
   }
 }
 // Clear Blurb colors
 function clearBlurbColors() {
-  for (var i = 0; i < displayBlurbList.length; i++) {
+  for (let i = 0; i < displayBlurbList.length; i++) {
     displayBlurbList[i].className = "displayBlurb";
   }
 }
 // Identify unique values in categories to display
 function unique(array, propertyName) {
-  var uniqueCat = []
-  var filteredObjects = array.filter((e, i) => array.findIndex(a => a[propertyName] === e[propertyName]) === i);
-  filteredObjects.forEach(function(element) {
-    uniqueCat.push(element["category"])
-  })
-  return uniqueCat
+  const uniqueCat = [];
+  let filteredObjects = array.filter(
+    (e, i) => array.findIndex((a) => a[propertyName] === e[propertyName]) === i
+  );
+  filteredObjects.forEach(function (element) {
+    uniqueCat.push(element["category"]);
+  });
+  return uniqueCat;
 }
 
 // Sort categories alphabetically to display on right side
-function rightSideSort(resultDB)  {
-  resultDB['blurbs'].sort(function(a, b) {
-    var objA = a.category.toUpperCase();
-    var objB = b.category.toUpperCase();
-    if (objA[0] < objB[0]) {
-      return -1;
-    }
-      else {
-        return 1;
-      }
-})
+function rightSideSort(resultDB) {
+  resultDB["blurbs"].sort(function (a, b) {
+    const objA = a.category.toUpperCase();
+    const objB = b.category.toUpperCase();
+    return objA[0] < objB[0] ? -1 : 1;
+  });
 }
 
 // If change in DB, updates right side display
-chrome.storage.onChanged.addListener(function() {
-       console.log('db updated')
-       displayRightSide()
-     });
+chrome.storage.onChanged.addListener(function () {
+  displayRightSide();
+});
 displayRightSide();
